1dd8a0edc297891e1030afb232366372b389f71d
Tyler Chi <itychi@gmail.com>
2018-03-08 20:59:57 -0800

    renders to screen

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//import a library that helps create a component
 
-AppRegistry.registerComponent('albums', () => App);
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a component
+
+const App = () => (
+    <Text> Some Text </Text>
+);
+
+//render it to the device
+//implicit return, automatically returns app.
+
+//the first argument is the name of the application. in this case, it is 'albums'
+//the second argument is a function that returns the first component of the application.
+
+AppRegistry.registerComponent('albums', () => App)
