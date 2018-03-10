5b3fd08f75efa3f0d8c059e528fa6af1233a3328
Tyler Chi <itychi@gmail.com>
2018-03-09 23:08:24 -0800

    header styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,39 @@
 
 //Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text , View } from 'react-native';
 
+
+//define the styling
 
 
 // Make a component
 const Header = () => {
-    return <Text> Header </Text>;
+
+    const { textStyle , viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}> Header </Text>
+        </View>
+    );
+}
+
+//the styles is basically one big object
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyCOntent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 5,
+        marginTop: 16,
+        paddingBottom: 5
+    },
+    textStyle: {
+        fontSize: 20
+    }
 }
 
 
